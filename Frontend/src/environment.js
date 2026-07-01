8324c2eb557cf028c3cc944444d94c73bd4e7f64
Ayush gupta <aayush3592@gmail.com>
2026-07-01 19:43:02 +0530

    Adding an environment variable

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,8 +1,7 @@
 let IS_PROD = true;
-const server = IS_PROD ?
-    "https://zoomify-videocall-meeting-app.onrender.com" :
-
-    "http://localhost:8000"
+const server = IS_PROD 
+    ? import.meta.env.VITE_SERVER_URL
+    : "http://localhost:8000"
 
 
 export default server;
